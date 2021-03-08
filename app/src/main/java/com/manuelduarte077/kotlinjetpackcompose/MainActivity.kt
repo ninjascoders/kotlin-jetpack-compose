5051b418ecc9c65329a4ba7b574c652d4c99fdb0
Manuel <carlosd99m@gmail.com>
2021-03-08 10:19:24 -0600

    finish

--- app/src/main/java/com/manuelduarte077/kotlinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/manuelduarte077/kotlinjetpackcompose/MainActivity.kt
@@ -3,38 +3,56 @@ package com.manuelduarte077.kotlinjetpackcompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.manuelduarte077.kotlinjetpackcompose.ui.theme.KotlinJetpackComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            KotlinJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PersonalData(name = "Manuel")
         }
 
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    private fun PersonalData(name: String) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KotlinJetpackComposeTheme {
-        Greeting("Android")
+        MaterialTheme() {
+            Column(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable._a49165e_o),
+                    contentDescription = "Manuel Duarte",
+                    modifier = Modifier.height(150.dp),
+                )
+                
+                Spacer(modifier = Modifier.height(16.dp))
+                
+                Text(text = "Mi nombre es $name", style = MaterialTheme.typography.h6)
+                Text(text = "manuelduarte077")
+                Text(text = "Hacker")
+            }
+        }
+        
     }
+
+    @Preview
+    @Composable
+    fun PreviewPersonalData() {
+        PersonalData(name = "Manuel")
+    }
+
 }
+
